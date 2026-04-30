4709408374884e18cc0ee8461c1e3f97f17cec40
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-04-30 22:25:39 +0700

    duplicate roll

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -51,3 +51,8 @@ public:
         {
             current = current->next;
         }
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
